a163c56ed5a2e85950a6c06e14f9fabf7758c702
Arthur Peter <arthurcristian.peter@gmail.com>
2025-05-14 21:31:34 +0300

    Created simple app to connect to the camera.

--- CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ CameraApp/app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.example.cameraapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.cameraapp.ui.theme.CameraAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Set the content to be a WebView inside Jetpack Compose
         setContent {
             CameraAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    WebViewContainer() // Custom composable for WebView
                 }
             }
         }
@@ -31,17 +31,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun WebViewContainer() {
+    // Create the WebView
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.useWideViewPort = true
+                settings.loadWithOverviewMode = true
+                settings.allowFileAccess = true
+                settings.mediaPlaybackRequiresUserGesture = false
+                webViewClient = WebViewClient()
+                webChromeClient = WebChromeClient()
+                loadUrl("http://192.168.4.1")
+            }
+        },
+        modifier = Modifier.fillMaxSize() // Make the WebView fill the screen
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CameraAppTheme {
-        Greeting("Android")
-    }
 }
